a089c6874ff1a7c625edb97e019181ac3286cd07
Zayn <zaynabid32@gmail.com>
2025-09-17 17:12:57 +0100

    Add minimum authentication with GitHub and NextAuth

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,3 @@
 export default async function Home() {
-  return <h1>Home Page</h1>;
+  return <h1 className="text-2xl">Home Page</h1>;
 }
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,54 @@
+import Link from 'next/link'
+import Image from 'next/image'
 import React from 'react'
+import { auth, signIn, signOut } from '@/auth'
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session=await auth();
+
   return (
-    <div>Navbar</div>
+    <div className='px-5 py-3 bg-white shadow-sm font-work-sans'>
+      <nav className='flex justify-between items-center'>
+        <Link href="/">
+          <Image src="/logo.png" alt="Logo" width={144} height={30} />
+        </Link>
+
+        <div className='flex items-center gap-5'>
+          {session && session?.user ? (
+            <>
+              <Link href="/dashboard" className='text-gray-600 hover:text-gray-800'>
+                <span>Create</span>
+              </Link>
+
+              <form action={async ()=> {
+                "use server"
+                await signOut({redirectTo: '/'})
+              }}>
+                <button type='submit'>
+                  <span>Logout</span>
+                </button>
+              </form>
+              
+
+              <Link href={`/user/${session?.user?.id}`}>
+                <span>{session?.user?.name}</span>
+              </Link>
+            </>
+            ):(
+              <form action={async ()=> {
+                "use server"
+                await signIn('github')
+              }}>
+                <button type='submit'>
+                  <span>Login</span>
+                </button>
+                
+              </form>
+            )
+          }
+        </div>
+      </nav>
+    </div>
   )
 }
 
